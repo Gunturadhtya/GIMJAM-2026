749218613237ee27cd81b8d4fc8dcccb9312971e
Gunturadhtya <gunturpdng@gmail.com>
2026-01-23 20:42:10 +0800

    create a shape resource

---
+++ Scripts/TrashShape.gd
@@ -0,0 +1,17 @@
+class_name TrashShape extends Resource
+
+@export var id: String = "base"
+#@export var text: Texture2D = null; # digunakan nanti setelah texture siap
+@export var color: Color = Color.WHITE
+@export var offset: Array[Vector2i] = [Vector2i(0,0)]
+
+func rotate():
+	var new_offsets = []
+	for points in offset:
+		new_offsets.append(Vector2i(-points.y, points.x))
+	offset = new_offsets
+
+func get_copy() -> TrashShape:
+	var copy = self.duplicate()
+	copy.offsets = offset.duplicate()
+	return copy
