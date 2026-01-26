cbbd40a18831cba529f87e1b7b64b622452be05a
Gunturadhtya <gunturpdng@gmail.com>
2026-01-26 20:55:08 +0800

    fix: ghost cursor rotated texture

--- Scripts/TrashShape.gd
+++ Scripts/TrashShape.gd
@@ -4,12 +4,15 @@ class_name TrashShape extends Resource
 @export var texture: Texture2D = null; # icon bar
 @export var atlas_coords: Array[Vector2i] = [Vector2i(0, 0)] # digunakan pada tile
 @export var offset: Array[Vector2i] = [Vector2i(0,0)]
+var rotated_degree: int = 0
 
 func rotate():
 	var new_offsets: Array[Vector2i] = []
 	for points in offset:
 		new_offsets.append(Vector2i(-points.y, points.x))
 	offset = new_offsets
+	rotated_degree = (rotated_degree + 90) % 360
+	print("Degree: ", rotated_degree)
 
 func get_copy() -> TrashShape:
 	var copy = self.duplicate(true)
--- Scripts/Visuals/GhostCursor.gd
+++ Scripts/Visuals/GhostCursor.gd
@@ -6,16 +6,14 @@ func update_visuals(shape: TrashShape):
 	for child in get_children():
 		child.queue_free()
 	
-	var level = get_tree().get_first_node_in_group("LevelManager")
-	
 	var atlas = PNGToAtlas.new()
-	
 	atlas.setup(shape.texture)
 	
 	var i = 0
 	for offset in shape.offset:
 		var sprite = Sprite2D.new()
 		sprite.texture = atlas.get_item_icon(shape.atlas_coords[i].x, shape.atlas_coords[i].y,1,1)
+		sprite.rotation_degrees = shape.rotated_degree
 		sprite.position = Vector2(offset) * CELL_SIZE
 		sprite.modulate.a = 0.5
 		i += 1
