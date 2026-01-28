9e022143b152bcd0aa2a6d1e7180656cfb61f28f
Gunturadhtya <gunturpdng@gmail.com>
2026-01-28 22:56:33 +0800

    add stage 2 setup

--- Scripts/StageData.gd
+++ Scripts/StageData.gd
@@ -3,5 +3,6 @@ class_name StageData extends Resource
 @export var title: String = "Title"
 @export var day: int = 1
 @export_multiline var description: String = ""
+@export_multiline var goal: String = ""
 @export var trash_goal: int = 0
 @export var grid_content: Dictionary[Vector2i, TrashShape]
--- Scripts/UI/LevelUI.gd
+++ Scripts/UI/LevelUI.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var desc_label = $Description
 
 func setup(data: StageData):
-	var text = "[color=#e4c892]%s[/color]\n" % data.title
-	text += "Day %d\n\n" % data.day
-	text += "[color=#9bd2d9]%s[/color]" % data.description
+	var text = "[color=#e4c892]%s[/color]\n\n" % data.title
+	text += "[color=#f5efe8]%s[/color]" % data.description
+	text += "[color=#9bd2d9]%s[/color]" % data.goal
 	desc_label.text = text
