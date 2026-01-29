7afc1564fe718f1ba874126c65dd69dff05dae31
Gunturadhtya <gunturpdng@gmail.com>
2026-01-29 19:39:21 +0800

    fix: add unmoveable object

--- Scripts/Systems/GridSystems.gd
+++ Scripts/Systems/GridSystems.gd
@@ -43,7 +43,8 @@ func _setup_astar() -> void:
 	# Static Walls
 	var walls = [
 		Vector2i(1, 0), Vector2i(2, 0), Vector2i(3, 0), Vector2i(4, 0),
-		Vector2i(4, 1), Vector2i(0, 0), Vector2i(0, 1), Vector2i(0, 2)
+		Vector2i(4, 1), Vector2i(0, 0), Vector2i(0, 1), Vector2i(0, 2),
+		Vector2i(5,0), Vector2i(1,5)
 	]
 	for wall in walls:
 		astar.set_point_solid(wall)
